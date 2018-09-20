9b24f3a5317dadedfa997e4194b4964c1914879a
Erin Green <erin.y.green@gmail.com>
2018-09-20 01:59:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0
   puts "The line is currently empty." 
 else 
-  current_line = "The line is currently"
+  current_line = "The line is currently:"
  katz_deli.each_with_index do |person, index|
-   current_line << "#{index}. #{person}"
+   current_line << " #{index}. #{person}"
 end
 end 
 end
