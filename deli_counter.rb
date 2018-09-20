73397b496c5daf812fd8c090a4cb9fc4d3d74777
Erin Green <erin.y.green@gmail.com>
2018-09-20 01:57:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,13 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length > 1 
-    puts "The line is currently" katz_deli[-1].insert(0, "#{katz_deli.length}.")
-    katz_deli.join(" ")
-   elsif katz_deli.length == 0
-  puts "The line is currently empty."
-  else katz_deli.length == 1 
-  puts "The line is currently 1. #{katz_deli[0]}"
-end 
-end 
+  if katz_deli.length == 0
+  puts "The line is currently empty." 
+else 
+  current_line = "The line is currently"
+ 
+end
+end
 
 def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
