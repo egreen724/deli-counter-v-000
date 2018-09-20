66ab4405f33a1ac82054f526e27def4e584b9694
Erin Green <erin.y.green@gmail.com>
2018-09-20 00:54:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,10 +12,10 @@ end
 def now_serving(katz_deli)
   if katz_deli.length > 0 
   puts "Now serving #{katz_deli[0]}."
+  katz_deli.shift 
   else 
   return "There is nobody waiting to be served!"
-end 
-  katz_deli.shift 
+  end 
 end
 
 
