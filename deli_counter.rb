b443a9190ec03eaf72a515711390dc388428efff
Erin Green <erin.y.green@gmail.com>
2018-09-20 01:32:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length > 1 
-  katz_deli.each{|person| puts "The line is currently #{katz_deli.length}. #{person}"}
+  katz_deli.each do |person| 
+    puts "The line is currently #{katz_deli.length}. #{person}" 
    elseif katz_deli.length == 0
   puts "The line is currently empty."
   else  katz_deli.length == 1 
