e162d4e85b6d4633a12b5234225162e7860d3d4c
Erin Green <erin.y.green@gmail.com>
2018-09-20 01:09:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push
-  puts "#{name}, you are #{katz_deli.length}"
+  puts "Welcome #{name}, you are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
