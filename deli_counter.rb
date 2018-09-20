d2a4c7777843336b778c94728e292a6cb388e64f
Erin Green <erin.y.green@gmail.com>
2018-09-20 01:02:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
   puts "The line is currently empty."
+elsif katz_deli.length == 1 
+puts "The line is currently 1. #{katz_deli[0]}"
 else puts "The line is currently 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
 end 
 end
