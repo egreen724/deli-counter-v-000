bc8edebfaf513d80875195f6f8685ceb0f082632
Erin Green <erin.y.green@gmail.com>
2018-09-20 01:16:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,10 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
   puts "The line is currently empty."
-  else katz_deli.length == 1 
-  puts "The line is currently  #{katz_deli[0]}"
+  elsif katz_deli.length == 1 
+  puts "The line is currently 1. #{katz_deli[0]}"
+  else 
+  puts "The line is currently #{katz_deli[index] + 1}. #{katz_deli.join
 end 
 end
 
